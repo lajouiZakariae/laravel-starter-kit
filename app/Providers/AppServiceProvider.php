91a2eb91dbe7f98c0b62dfdf85c0a2a1cc96a595
Zakariae Lajoui <80148036+lajouiZakariae@users.noreply.github.com>
2025-09-07 04:03:19 +0100

    refactor: UserContext binding in AppServiceProvider
    - Moved UserContext binding to the class property for improved readability and maintainability.
    - Remove the previous binding method in the register function.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,12 +7,14 @@ use App\Interfaces\UserContextInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider {
+    public $bindings = [
+        UserContextInterface::class => UserContext::class,
+    ];
+
     /**
      * Register any application services.
      */
-    public function register(): void {
-        app()->bind(UserContextInterface::class, UserContext::class);
-    }
+    public function register(): void {}
 
     /**
      * Bootstrap any application services.
