d13d0d683f39e28b5de4fd390e6f8a5a106e52f8
Zakariae Lajoui <80148036+lajouiZakariae@users.noreply.github.com>
2025-10-28 22:36:52 +0100

    feat: add query scope for searching by name

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -2,11 +2,15 @@
 
 namespace App\Models;
 
+use App\Models\Scopes\ActiveCountry;
+use Illuminate\Database\Eloquent\Attributes\ScopedBy;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\Translatable\HasTranslations;
 
+#[ScopedBy(ActiveCountry::class)]
 class Country extends Model implements HasMedia {
     use HasTranslations;
     use InteractsWithMedia;
@@ -35,5 +39,18 @@ class Country extends Model implements HasMedia {
     public function registerMediaCollections(): void {
         $this->addMediaCollection('flags')
             ->singleFile();
+    }
+
+    public function scopeSearch(Builder $query, string $search) {
+        return $query->where(function (Builder $q) use ($search) {
+            $q->whereJsonContains('common_name->en', $search)
+                ->orWhereJsonContains('common_name->ar', $search)
+                ->orWhereJsonContains('common_name->fr', $search)
+                ->orWhereJsonContains('official_name->en', $search)
+                ->orWhereJsonContains('official_name->ar', $search)
+                ->orWhereJsonContains('official_name->fr', $search)
+                ->orWhere('iso_3166_1_alpha2', 'like', "%{$search}%")
+                ->orWhere('iso_3166_1_alpha3', 'like', "%{$search}%");
+        });
     }
 }
