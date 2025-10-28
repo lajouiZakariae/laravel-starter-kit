f383cfa77b9f615ed95b6fa30e0a40afb213fc0f
Zakariae Lajoui <80148036+lajouiZakariae@users.noreply.github.com>
2025-10-28 22:36:25 +0100

    feat: add pagination data class

---
+++ app/Data/PaginationData.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Data;
+
+use Spatie\LaravelData\Data;
+
+class PaginationData extends Data {
+    public function __construct(
+        public readonly bool $paginate = true,
+        public readonly int $perPage = 15,
+    ) {}
+}
