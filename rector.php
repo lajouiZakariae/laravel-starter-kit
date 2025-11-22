57225d0e8eeace3644d23dc45c504c18f03dd7c1
Zakariae Lajoui <80148036+lajouiZakariae@users.noreply.github.com>
2025-11-23 00:50:58 +0100

    chore: Update dev dependencies and Rector config
    Added driftingly/rector-laravel to require-dev and updated rector/rector to v2.2.8 in composer.json. Updated rector.php configuration. Dependency versions in composer.lock were updated accordingly.

--- rector.php
+++ rector.php
@@ -3,19 +3,56 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use RectorLaravel\Rector\Empty_\EmptyToBlankAndFilledFuncRector;
+use RectorLaravel\Rector\FuncCall\ConfigToTypedConfigMethodCallRector;
+use RectorLaravel\Rector\FuncCall\HelperFuncCallToFacadeClassRector;
+use RectorLaravel\Rector\FuncCall\RemoveDumpDataDeadCodeRector;
+use RectorLaravel\Rector\MethodCall\WhereToWhereLikeRector;
+use RectorLaravel\Rector\StaticCall\RequestStaticValidateToInjectRector;
+use RectorLaravel\Set\LaravelSetList;
+use RectorLaravel\Set\LaravelSetProvider;
 
 return RectorConfig::configure()
     ->withPaths([
         __DIR__ . '/app',
-        __DIR__ . '/bootstrap/app.php',
+        __DIR__ . '/bootstrap',
         __DIR__ . '/database',
         __DIR__ . '/config',
+        __DIR__ . '/lang',
         __DIR__ . '/public',
         __DIR__ . '/resources',
         __DIR__ . '/routes',
         __DIR__ . '/tests',
     ])
+    ->withImportNames(
+        importDocBlockNames: false,
+        importShortClasses: true,
+        importNames: true,
+        removeUnusedImports: true,
+    )
+    ->withSetProviders(LaravelSetProvider::class)
+    ->withComposerBased(laravel: true)
+    ->withRules([
+        HelperFuncCallToFacadeClassRector::class,
+        RequestStaticValidateToInjectRector::class,
+        ConfigToTypedConfigMethodCallRector::class,
+        EmptyToBlankAndFilledFuncRector::class,
+        RemoveDumpDataDeadCodeRector::class,
+        WhereToWhereLikeRector::class,
+    ])
+    ->withSets([
+        LaravelSetList::LARAVEL_CODE_QUALITY,
+        LaravelSetList::LARAVEL_COLLECTION,
+        LaravelSetList::LARAVEL_TYPE_DECLARATIONS,
+        LaravelSetList::LARAVEL_ARRAYACCESS_TO_METHOD_CALL,
+        LaravelSetList::LARAVEL_ELOQUENT_MAGIC_METHOD_TO_QUERY_BUILDER,
+        LaravelSetList::LARAVEL_ARRAY_STR_FUNCTION_TO_STATIC_CALL,
+        LaravelSetList::LARAVEL_CONTAINER_STRING_TO_FULLY_QUALIFIED_NAME,
+        LaravelSetList::LARAVEL_FACADE_ALIASES_TO_FULL_NAMES,
+        LaravelSetList::LARAVEL_FACTORIES,
+        LaravelSetList::LARAVEL_LEGACY_FACTORIES_TO_CLASSES,
+    ])
     ->withPhpSets()
-    ->withTypeCoverageLevel(0)
-    ->withDeadCodeLevel(0)
-    ->withCodeQualityLevel(0);
+    ->withTypeCoverageLevel(10)
+    ->withDeadCodeLevel(10)
+    ->withCodeQualityLevel(10);
