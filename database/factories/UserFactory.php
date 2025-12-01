b7e24091c296fc3d567b4a2b7370a984f7a73645
Zakariae Lajoui <lajoui.zakariae.1@gmail.com>
2025-12-01 01:21:56 +0100

    refactor: Simplify UserFactory password generation
    Removed dependency on Hasher and constructor injection in UserFactory. Password is now set to a plain string 'password' for generated users, streamlining the factory for testing purposes.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,9 +3,7 @@
 namespace Database\Factories;
 
 use App\ValueObjects\PhoneNumber;
-use Illuminate\Contracts\Hashing\Hasher;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
 
 /**
@@ -18,16 +16,6 @@ class UserFactory extends Factory {
     protected static ?string $password = null;
 
     /**
-     * Create a new factory instance.
-     *
-     * @param  int|null  $count
-     * @param  \UnitEnum|string|null  $connection
-     */
-    public function __construct($count, ?Collection $states = null, ?Collection $has = null, ?Collection $for = null, ?Collection $afterMaking = null, ?Collection $afterCreating = null, $connection = null, ?Collection $recycle = null, ?bool $expandRelationships = null, array $excludeRelationships = [], private readonly ?Hasher $hasher = null) {
-        parent::__construct($count, $states, $has, $for, $afterMaking, $afterCreating, $connection, $recycle, $expandRelationships, $excludeRelationships);
-    }
-
-    /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
@@ -38,7 +26,7 @@ class UserFactory extends Factory {
             'last_name' => fake()->lastName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= $this->hasher->make('password'),
+            'password' => 'password',
             'remember_token' => Str::random(10),
             'phone_number_country_code' => fake()->countryCode(),
             'phone_number' => new PhoneNumber(fake()->e164PhoneNumber()),
