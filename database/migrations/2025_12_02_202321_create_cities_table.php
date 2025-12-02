a29eeb81e7cf8e815328fea58add9efc7a467dac
Zakariae Lajoui <lajoui.zakariae.1@gmail.com>
2025-12-02 23:36:59 +0100

    feat: Add return type to cities table migration closure
    Specified the return type 'void' for the closure in the cities table migration to improve type safety and code clarity.

--- database/migrations/2025_12_02_202321_create_cities_table.php
+++ database/migrations/2025_12_02_202321_create_cities_table.php
@@ -9,7 +9,7 @@ return new class extends Migration {
      * Run the migrations.
      */
     public function up(): void {
-        Schema::create('cities', function (Blueprint $table) {
+        Schema::create('cities', function (Blueprint $table): void {
             $table->id();
             $table->foreignId('country_id')->constrained()->cascadeOnDelete();
             $table->string('name');
