a823a06aaccb867e1067407dffc38abfa75b19e1
Zakariae Lajoui <lajoui.zakariae.1@gmail.com>
2026-06-01 14:48:09 +0100

    fix: import missing

--- routes/api/auth/email-verification.php
+++ routes/api/auth/email-verification.php
@@ -3,7 +3,9 @@
 use App\Http\Controllers\Api\Auth\EmailVerificationController;
 use Illuminate\Support\Facades\Route;
 
-// OTP Based Email Verification Routes
-Route::post('email/otp/send', [EmailVerificationController::class, 'sendVerificationEmail'])->name('email.send')->middleware('throttle:send-verification-email');
+Route::middleware('auth:api')->group(function (): void {
+    // OTP Based Email Verification Routes
+    Route::post('email/otp/send', [EmailVerificationController::class, 'sendVerificationEmail'])->name('email.send')->middleware('throttle:send-verification-email');
 
-Route::post('email/otp/verify', [EmailVerificationController::class, 'verifyEmail'])->name('email.verify');
+    Route::post('email/otp/verify', [EmailVerificationController::class, 'verifyEmail'])->name('email.verify');
+});
--- routes/api/auth/index.php
+++ routes/api/auth/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\Auth\JWTAuthController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
